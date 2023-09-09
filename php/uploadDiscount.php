5c25dafea0a5aaea5438608449ff5ae29c6bcb8d
VasilisKyriakos <billkir676@gmail.com>
2023-09-09 15:13:12 +0300

    commit

--- php/uploadDiscount.php
+++ php/uploadDiscount.php
@@ -10,28 +10,19 @@ $response = [
     "message" => ""
 ];
 
-// Check if data exists
-if(isset($_POST['user_id'], $_POST['shop_id'], $_POST['product_name'], $_POST['price'])) {
-    $userId = $_POST['user_id'];
-    $shopId = $_POST['shop_id'];
-    $productName = $_POST['product_name'];
-    $price = $_POST['price'];
+$userId = $_POST['user_id'];
+$shopId = $_POST['shop_id'];
+$productName = $_POST['product_name'];
+$price = $_POST['price'];
 
-    // Use prepared statements to avoid SQL injection
-    $stmt = $link->prepare("INSERT INTO discounts (user_id, shop_id, product_name, price ) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("iisss", $userId, $shopId, $productName, $price);
 
-    if ($stmt->execute()) {
-        $response["status"] = "success";
-        $response["message"] = "Discount uploaded successfully!";
-    } else {
-        $response["message"] = "Error: " . $stmt->error;
-    }
+$sql = "INSERT INTO discounts (user_id, shop_id, product_name, price) VALUES ('$userId', '$shopId', '$productName', '$price')";
 
-    $stmt->close();
-
+if (mysqli_query($link, $sql)) {
+    $response["status"] = "success";
+    $response["message"] = "Discount uploaded successfully!";
 } else {
-    $response["message"] = "Required fields are missing.";
+    $response["message"] = "Error: " . mysqli_error($link);
 }
 
 echo json_encode($response);
