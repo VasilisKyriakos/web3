2da259890aeeb051fe5ff196e544c7ed15963881
VasilisKyriakos <billkir676@gmail.com>
2023-09-10 18:03:21 +0300

    commit

--- php/updateLikes.php
+++ php/updateLikes.php
@@ -8,20 +8,40 @@ error_reporting(E_ALL);
 $response = ['status' => 'error', 'message' => 'Unknown Error'];
 
 if (isset($_POST['discount_id']) && isset($_POST['type'])) {
+    
     $discount_id = mysqli_real_escape_string($link, $_POST['discount_id']);
     $type = $_POST['type'];
     $column = $type === 'like' ? 'likes' : 'dislikes';
-    
-    $query = "UPDATE discounts SET $column = $column + 1 WHERE discount_id = $discount_id";
-    $result = mysqli_query($link, $query);
 
-    if ($result) {
+        $query = "UPDATE discounts SET $column = $column + 1 WHERE discount_id = $discount_id";
+        if(!mysqli_query($link, $query)) {
+            throw new Exception("Failed to update like/dislike count: " . mysqli_error($link));
+        }
+
+        // Get the user_id of the recommending user from the discount
+        $getUserQuery = "SELECT user_id FROM discounts WHERE discount_id = $discount_id";
+        $result = mysqli_query($link, $getUserQuery);
+
+    
+        $data = mysqli_fetch_assoc($result);
+        $recommendingUserId = $data['user_id'];
+
+        // Update points based on the action
+        if ($type === 'like') {
+            $pointsQuery = "UPDATE users SET points = points + 5 WHERE id = $recommendingUserId";
+        } else {
+            $pointsQuery = "UPDATE users SET points = points - 1 WHERE id = $recommendingUserId";
+        }
+
+        if(!mysqli_query($link, $pointsQuery)) {
+            throw new Exception("Failed to update user points: " . mysqli_error($link));
+        }
+
+        // Commit the transaction
+        mysqli_commit($link);
         $response['status'] = 'success';
         $response['message'] = 'Updated Successfully';
-    } else {
-        $response['message'] = "Database update failed: " . mysqli_error($link);
-    }
-}
 
+}
 echo json_encode($response);
 ?>
--- php/uploadDiscount.php
+++ php/uploadDiscount.php
@@ -11,7 +11,7 @@ error_reporting(E_ALL);
 
 $response = array(
     'status' => 'error',
-    'message' => 'User ID is not set.'
+    'message' => ''
 );
 
 if(isset($_POST['user_id'],$_POST['shop_id'],$_POST['product_name'],$_POST['product_price'])) {
@@ -28,10 +28,10 @@ if(isset($_POST['user_id'],$_POST['shop_id'],$_POST['product_name'],$_POST['prod
 
     // Check the average price of the previous day
     $yesterday = date("Y-m-d", strtotime("-1 day"));
-    $queryPriceYesterday = "SELECT price as avgPrice FROM prices WHERE product_id = '$productId' AND date = '$yesterday'";
+    $queryPriceYesterday = "SELECT price FROM prices WHERE product_id = '$productId' AND date = '$yesterday'";
     $resultPriceYesterday = mysqli_query($link, $queryPriceYesterday);
     $priceDataYesterday = mysqli_fetch_assoc($resultPriceYesterday);
-    $avgPriceYesterday = $priceDataYesterday['avgPrice'];
+    $avgPriceYesterday = $priceDataYesterday['price'];
 
     // Check the average price of the previous week
     $lastWeekStart = date("Y-m-d", strtotime("-7 days"));
@@ -48,14 +48,6 @@ if(isset($_POST['user_id'],$_POST['shop_id'],$_POST['product_name'],$_POST['prod
     $response['calculations']['avgPriceLastWeek'] = $avgPriceLastWeek;
     $response['calculations']['80%OfYesterday'] = 0.8 * $avgPriceYesterday;
     $response['calculations']['80%OfLastWeek'] = 0.8 * $avgPriceLastWeek;
-
-
-        // Append calculations to the response message
-    $response['message'] .= "\nUser Price: " . $response['calculations']['userPrice'];
-    $response['message'] .= "\nAverage Price Yesterday: " . $response['calculations']['avgPriceYesterday'];
-    $response['message'] .= "\nAverage Price Last Week: " . $response['calculations']['avgPriceLastWeek'];
-    $response['message'] .= "\n80% Of Average Price Yesterday: " . $response['calculations']['80%OfYesterday'];
-    $response['message'] .= "\n80% Of Average Price Last Week: " . $response['calculations']['80%OfLastWeek'];
 
 
     if ($productPrice <= (0.8 * $avgPriceYesterday)) {
